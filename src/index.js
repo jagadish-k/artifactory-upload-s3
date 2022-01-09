3285ba48c57f49161d7e98826a3cccc0986d2b51
Jagadish, K <jagadish.kasi@cleartax.in>
2022-01-09 21:18:27 +0530

    set up env configs for AWS

--- src/index.js
+++ src/index.js
@@ -1,21 +1,23 @@
 const core = require('@actions/core');
-const github = require('@actions/github');
-const fs = require("fs");
-const path = require("path");
+const fs = require('fs');
+const path = require('path');
 const AWS = require('aws-sdk');
-// Set the Region 
-AWS.config.update({ region: core.getInput('aws-region') });
+// Set the Region
+AWS.config.update({
+  region: core.getInput('aws-region'),
+  accessKeyId: core.getInput('aws-access-key-id'),
+  secretAccessKey: core.getInput('aws-secret-access-key'),
+});
 
 try {
   const artifactToUpload = core.getInput('artifact');
   if (fs.lstatSync(artifactToUpload).isDirectory()) {
-    throw new Error('"artifact" must be a single file. Directories are not supported.');
+    throw new Error(
+      '"artifact" must be a single file. Directories are not supported.'
+    );
   } else {
     // const spacesEndpoint = new aws.Endpoint(process.env.S3_ENDPOINT);
-    const s3 = new AWS.S3({
-      accessKeyId: process.env.S3_ACCESS_KEY_ID,
-      secretAccessKey: process.env.S3_SECRET_ACCESS_KEY,
-    });
+    const s3 = new AWS.S3({});
     // get `app-name` the root app for which the build is being done.
     const appName = core.getInput('app-name');
     const destDir = core.getInput('destination-dir');
